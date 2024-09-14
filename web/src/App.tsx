da063520f0c960fc46e5f584a946813a331e5790
teavver <jerzyx11@gmail.com>
2024-09-14 22:35:50 +0200

    add webapp template

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="flex flex-col gap-3 w-screen h-screen m-0 p-0 px-[30%] bg-black text-white">
+            <p>ZKML</p>
+        </div>
+    )
+}
+
+export default App

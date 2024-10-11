45cf1a95683f74f60224754ce374b1b3a16faabf
teavver <jerzyx11@gmail.com>
2024-10-11 14:57:28 -0500

    ezkl home

--- web/src/routes/Home.tsx
+++ web/src/routes/Home.tsx
@@ -1,11 +1,17 @@
 import { Nav } from "../components/Nav"
 import { Content } from "../components/Content"
 import { routes } from "../router"
+import { Link } from "react-router-dom"
 
 export const Home = () => {
-    return (
-      <Content>
-        <Nav title={routes.home.title} />
-      </Content>
-    )
+  return (
+    <Content>
+      <Nav title={routes.home.title} />
+      <p>
+        Testing the&nbsp;
+        <Link target={"_blank"} to={"https://github.com/zkonduit/ezkl"}>EZKL</Link>
+        &nbsp;library on light models and low-spec hardware.
+      </p>
+    </Content>
+  )
 }
